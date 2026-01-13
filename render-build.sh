264fbb618818ba4b3739814b0929825a6857dcec
8688-harshu <harshu.ram824@gmail.com>
2026-01-13 11:27:29 +0530

    Fix: Hardcode Playwright path in scripts

--- render-build.sh
+++ render-build.sh
@@ -2,8 +2,11 @@
 # Exit on error
 set -o errexit
 
+# FORCE the browser path to be in the project directory
+export PLAYWRIGHT_BROWSERS_PATH=0
+
 npm install
 npm run build:prod
 
-# Install Playwright Browsers
+# Install Playwright Browsers to ./node_modules/
 npx playwright install chromium
